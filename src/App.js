6b30a94ae2fe4b8c43118ff7ad3a4e309c677837
Harshitha <palemharshithagmail.com>
2024-11-27 16:44:05 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Singlecomponent from './examples/singlecomponent';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Singlecomponent/>
+
+    </div>
+  );
+}
+
+export default App;
